064617f7aaba6ce7c0b86da5eef966ada430a227
George Victor <id0ubl3g@gmail.com>
2025-02-12 22:20:32 -0300

    feat: add styled output functions for user interactions

---
+++ config/colors_config.py
@@ -0,0 +1,8 @@
+RESET = "\033[0m"
+BOLD = "\033[1m"
+WHITE = "\033[37m"
+YELLOW = "\033[33m"
+BRIGHT_YELLOW = "\033[1;33m"
+RED = "\033[91m"
+ORANGE = "\033[38;5;208m"
+GREEN = "\033[92m"
---
+++ src/utils/style_outputs.py
@@ -0,0 +1,43 @@
+from config.collors_config import WHITE, YELLOW, ORANGE, RED, GREEN, RESET, BOLD
+
+def print_welcome_message() -> None:
+    print(rf'''{GREEN}{BOLD}
+8b   d8 w                .d88b.                               
+8YbmdP8 w 8d8b. .d88b    YPwww. .d8b 8d8b .d88 88b. .d88b 8d8b
+8  "  8 8 8P Y8 8.dP'        d8 8    8P   8  8 8  8 8.dP' 8P  
+8     8 8 8   8 `Y88P    `Y88P' `Y8P 8    `Y88 88P' `Y88P 8   
+                                               8             
+
+        {RESET}{WHITE}Powerful {GREEN}{BOLD}MineScraper{WHITE} for extracting and processing data.
+            {RESET}{GREEN}
+    [*]__author__: {RESET}George Victor | @id0ubl3g{GREEN}
+    [*]__github__: {RESET}github.com/id0ubl3g/mine-scraper{GREEN}
+    [*]__usage__: {YELLOW}python3{RESET} run.py --localhost {GREEN}<True/False>{RESET}{WHITE} | Configure proxy behavior{RESET}''')
+
+def print_select_scraping_mode() -> None:
+    print(f'\n{GREEN}[+]{RESET}{BOLD} Select a scraping mode:{RESET}\n')
+    print(f'{GREEN}[1]{RESET} Instagram: Collect emails, phone numbers and usernames from profiles')
+
+def print_interrupted_message() -> None:
+    print(f'\n{ORANGE}[!]{RESET} Operation interrupted by user. Exiting gracefully...')
+
+def print_invalid_value(message: str) -> None:
+    print(f'\n{ORANGE}[i]{RESET} Invalid value: {WHITE}{message}{RESET}')
+
+def print_error_unexpected() -> None:
+    print(f'\n{RED}[x]{RESET} An unexpected error occurred.')
+
+def print_error_choosing_proxy() -> None:
+    print(f'\n{RED}[x]{RESET} An error occurred while choosing the proxy.')
+
+def print_error_connect_proxy(chosen_city) -> None:
+    print(f'{RED}[x]{RESET} An error occurred while connect to the proxy: {chosen_city}')
+
+def print_error_testing_proxy(chosen_city) -> None:
+    print(f'\n{RED}[x]{RESET} An error occurred while testing the proxy: {chosen_city}')
+
+def print_proxy_online(chosen_city, chosen_proxy) -> None:
+    print(f'\n{GREEN}•{RESET}{WHITE} Proxy: {chosen_city} | {chosen_proxy} (Online){RESET}')
+
+def print_proxy_disconcerted(chosen_city, chosen_proxy) -> None:
+    print(f'\n{RED}•{RESET}{WHITE} Proxy: {chosen_city} | {chosen_proxy} (Disconcerted){RESET}')
